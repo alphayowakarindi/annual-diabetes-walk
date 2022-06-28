b6ab550ab64dc01ac781c38cd094981bca32ced3
Alphy Gacheru <alphygacheru@gmail.com>
2022-06-28 17:11:31 +0300

    Use Js to hide overlay when a nav link is clicked

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.querySelector(".fa-bars");
 const mobileOverlay = document.querySelector("#mobile-overlay");
 const xMark = document.querySelector(".fa-xmark");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
 
 hamburgerMenu.addEventListener(
   "click",
@@ -8,3 +9,10 @@ hamburgerMenu.addEventListener(
 );
 
 xMark.addEventListener("click", () => (mobileOverlay.style.display = "none"));
+
+mobileNavLinks.forEach((mobileNavLink) => {
+  mobileNavLink.addEventListener(
+    "click",
+    () => (mobileOverlay.style.display = "none")
+  );
+});
