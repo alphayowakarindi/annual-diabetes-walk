696767e6598c0c303f57e8f1a108d0639268ea7e
Alphy Gacheru <alphygacheru@gmail.com>
2022-06-28 16:58:32 +0300

    Hide overlay when xMark is clicked

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const hamburgerMenu = document.querySelector(".fa-bars");
 const mobileOverlay = document.querySelector("#mobile-overlay");
+const xMark = document.querySelector(".fa-xmark");
 
 hamburgerMenu.addEventListener(
   "click",
   () => (mobileOverlay.style.display = "block")
 );
+
+xMark.addEventListener("click", () => (mobileOverlay.style.display = "none"));
