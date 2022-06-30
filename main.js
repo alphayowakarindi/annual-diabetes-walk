96face3b1eb169451eeda570e308fcb5fa281985
Alphy Gacheru <alphygacheru@gmail.com>
2022-06-30 14:49:13 +0300

    Update html and style for past walks section

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const hamburgerMenu = document.querySelector(".fa-bars");
-const mobileOverlay = document.querySelector("#mobile-overlay");
-const xMark = document.querySelector(".fa-xmark");
-const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
+const hamburgerMenu = document.querySelector('.fa-bars');
+const mobileOverlay = document.querySelector('#mobile-overlay');
+const xMark = document.querySelector('.fa-xmark');
+const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
 
-hamburgerMenu.addEventListener("click", () => {
-  mobileOverlay.style.display = "block";
+hamburgerMenu.addEventListener('click', () => {
+  mobileOverlay.style.display = 'block';
 });
 
-xMark.addEventListener("click", () => {
-  mobileOverlay.style.display = "none";
+xMark.addEventListener('click', () => {
+  mobileOverlay.style.display = 'none';
 });
 
 mobileNavLinks.forEach((mobileNavLink) => {
-  mobileNavLink.addEventListener("click", () => {
-    mobileOverlay.style.display = "none";
+  mobileNavLink.addEventListener('click', () => {
+    mobileOverlay.style.display = 'none';
   });
 });
 
@@ -21,68 +21,68 @@ mobileNavLinks.forEach((mobileNavLink) => {
 const featuredSpeakers = [
   {
     id: 1,
-    name: "Mutahi Kagwe",
-    subtitle: "Cabinet minister for health in the rebublic of Kenya",
+    name: 'Mutahi Kagwe',
+    subtitle: 'Cabinet minister for health in the rebublic of Kenya',
     description:
-      "A Kenyan tenured politician and currently serves as the Cabinet Secretary for Health in the Republic of Kenya.",
-    bottomImageUrl: "images/speakerbg.jpg",
-    topImageUrl: "images/mimister-of-health.jpeg",
+      'A Kenyan tenured politician and currently serves as the Cabinet Secretary for Health in the Republic of Kenya.',
+    bottomImageUrl: 'images/speakerbg.jpg',
+    topImageUrl: 'images/mimister-of-health.jpeg',
   },
 
   {
     id: 2,
-    name: "Peter Ndegwa",
-    subtitle: "CEO of Safaricom telecomunication company",
+    name: 'Peter Ndegwa',
+    subtitle: 'CEO of Safaricom telecomunication company',
     description:
-      "An experienced Board level leader with a wealth of experience in General Management.",
-    bottomImageUrl: "images/speakerbg.jpg",
-    topImageUrl: "images/safaricom-ceo.jpg",
+      'An experienced Board level leader with a wealth of experience in General Management.',
+    bottomImageUrl: 'images/speakerbg.jpg',
+    topImageUrl: 'images/safaricom-ceo.jpg',
   },
 
   {
     id: 3,
-    name: "William Ruto",
-    subtitle: "Deputy president of the republic of Kenya",
+    name: 'William Ruto',
+    subtitle: 'Deputy president of the republic of Kenya',
     description:
-      "The Deputy President of Kenya. Before this, he was a Member of Parliament for the Eldoret North Constituency since 1997.",
-    bottomImageUrl: "images/speakerbg.jpg",
-    topImageUrl: "images/deputy-president.jpg",
+      'The Deputy President of Kenya. Before this, he was a Member of Parliament for the Eldoret North Constituency since 1997.',
+    bottomImageUrl: 'images/speakerbg.jpg',
+    topImageUrl: 'images/deputy-president.jpg',
   },
 
   {
     id: 4,
-    name: "Uhuru Kenyatta",
-    subtitle: "President of the republic of Kenya",
+    name: 'Uhuru Kenyatta',
+    subtitle: 'President of the republic of Kenya',
     description:
-      "The Deputy President of Kenya. Before this, he was a Member of Parliament for the GatunduConstituency since 2001.",
-    bottomImageUrl: "images/speakerbg.jpg",
-    topImageUrl: "images/president.jpg",
+      'The Deputy President of Kenya. Before this, he was a Member of Parliament for the GatunduConstituency since 2001.',
+    bottomImageUrl: 'images/speakerbg.jpg',
+    topImageUrl: 'images/president.jpg',
   },
 
   {
     id: 5,
-    name: "Pastor Nganga",
-    subtitle: "Pastor of Neno Evangelism Church",
+    name: 'Pastor Nganga',
+    subtitle: 'Pastor of Neno Evangelism Church',
     description:
-      "A Kenyan preacher, apostle, businessman and the founder of Neno Evangelism Centre.",
-    bottomImageUrl: "images/speakerbg.jpg",
-    topImageUrl: "images/pastor-nganga.jpg",
+      'A Kenyan preacher, apostle, businessman and the founder of Neno Evangelism Centre.',
+    bottomImageUrl: 'images/speakerbg.jpg',
+    topImageUrl: 'images/pastor-nganga.jpg',
   },
   {
     id: 6,
-    name: "Mike Sonko",
-    subtitle: "Governor of Nairobi",
+    name: 'Mike Sonko',
+    subtitle: 'Governor of Nairobi',
     description:
-      "Mr Sonko started his political journey in 2010, when he contested a parliamentary by-election.",
-    bottomImageUrl: "images/speakerbg.jpg",
-    topImageUrl: "images/mike-sonko.jpg",
+      'Mr Sonko started his political journey in 2010, when he contested a parliamentary by-election.',
+    bottomImageUrl: 'images/speakerbg.jpg',
+    topImageUrl: 'images/mike-sonko.jpg',
   },
 ];
 
-const featuredSpeakersElement = document.querySelector(".speakers");
+const featuredSpeakersElement = document.querySelector('.speakers');
 
-window.addEventListener("DOMContentLoaded", () => {
-  let featuredSpeakersHtmlVersion = "";
+window.addEventListener('DOMContentLoaded', () => {
+  let featuredSpeakersHtmlVersion = '';
   for (let i = 0; i < featuredSpeakers.length; i += 1) {
     featuredSpeakersHtmlVersion += ` 
     <div class="speaker">
